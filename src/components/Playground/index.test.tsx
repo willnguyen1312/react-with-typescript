7ecd43c4d9052cd657d22807b5c0c28bf903ebc7
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-07-18 12:15:28 +0700

    Aha

--- src/components/Playground/index.test.tsx
+++ src/components/Playground/index.test.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import {useMousePosition} from './useMousePosition'
-import 'jest-dom/extend-expect'
+import React from "react";
+import { useMousePosition } from "./useMousePosition";
+import "jest-dom/extend-expect";
 import Mouse from ".";
 import { render } from "@testing-library/react";
 
-jest.mock('./useMousePosition.tsx', () => ({
+jest.mock("./useMousePosition.tsx", () => ({
   useMousePosition: jest.fn()
-}))
+}));
 
 test("Mouse component should work", () => {
   const xPosition = 5;
   const yPosition = 6;
-  (useMousePosition as jest.Mock).mockReturnValueOnce({x: xPosition, y: yPosition})
-  const {getByTestId, container} = render(<Mouse />);
+  (useMousePosition as jest.Mock).mockReturnValueOnce({
+    x: xPosition,
+    y: yPosition
+  });
+  const { getByTestId, container } = render(<Mouse />);
 
   // expect(getByTestId('xPosition').innerText).toBe(xPosition)
-  expect(getByTestId('xPosition')).toHaveTextContent(xPosition.toString())
-  expect(getByTestId('yPosition')).toHaveTextContent(yPosition.toString())
+  expect(getByTestId("xPosition")).toHaveTextContent(xPosition.toString());
+  expect(getByTestId("yPosition")).toHaveTextContent(yPosition.toString());
 });
