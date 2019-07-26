d750081122142c4097178919ff6b8d8fc6969375
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-07-26 16:27:05 +0700

    Oho

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { FC } from "react";
 import HOC from "./components/HOC";
 import RPS from "./components/RPS";
 // import Playground from "./components/Playground";
 import Playground from "./components/Sample";
+import cover from "./axo.jpg";
+
+const Hello = () => <h1>Hello</h1>;
+
+const Hi: FC<{ AAA: any }> = ({ AAA }) => <div>{AAA}</div>;
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hi hi</h1>
+      <Hi AAA={<Hello />} />
       {/* <HOC /> */}
       {/* <RPS /> */}
-      <Playground />
-      <div>
-        <img src="https://picsum.photos/id/10/2500/1667" />
-      </div>
+      {/* <Playground /> */}
     </div>
   );
 };
