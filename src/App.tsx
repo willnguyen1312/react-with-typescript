ef76e599c8a2b54b9a4a1916984378a3017e6113
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-08-09 11:18:37 +0700

    Cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from "react";
 import HOC from "./components/HOC";
 import RPS from "./components/RPS";
-// import Playground from "./components/Playground";
-import Playground from "./components/Sample";
+import Playground from "./components/Playground";
+// import Playground from "./components/Sample";
 import cover from "./axo.jpg";
 
 const Hello = () => <h1>Hello</h1>;
@@ -12,10 +12,10 @@ const Hi: FC<{ AAA: any }> = ({ AAA }) => <div>{AAA}</div>;
 const App = () => {
   return (
     <div className="App">
-      <Hi AAA={<Hello />} />
+      {/* <Hi AAA={<Hello />} /> */}
       {/* <HOC /> */}
       {/* <RPS /> */}
-      {/* <Playground /> */}
+      <Playground />
     </div>
   );
 };
--- src/components/Playground/index.tsx
+++ src/components/Playground/index.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import imageSrc from "../../images/axo.jpg";
 
-export default class Playground extends React.Component {
-  renderSomethingOdd = () => {
-    debugger;
-    return <h1>Fuck Yeah</h1>;
+const Playground = () => {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const image = new Image();
+  image.src = imageSrc;
+
+  image.onload = () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+
+    const canvasContex = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    canvasContex.drawImage(image, 0, 0);
+
+    canvas.toBlob(blob => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(blob as Blob);
+
+      fileReader.onload = () => {
+        imageRef.current!.src = fileReader.result as string;
+      };
+    });
   };
-  render() {
-    return (
-      <>
-        <h1>Hello</h1>
-        {this.renderSomethingOdd()}
-      </>
-    );
-  }
-}
+
+  return (
+    <div>
+      <h1>Image</h1>
+      <img src={imageSrc} />
+      <img ref={imageRef} />
+    </div>
+  );
+};
+
+export default Playground;
