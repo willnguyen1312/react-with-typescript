11e9b4cd7f673730e134951f21926d7f8caa891a
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-09-16 11:17:32 +0700

    Oho

--- src/sample.test.tsx
+++ src/sample.test.tsx
@@ -1,69 +1,8 @@
 import React, { useState } from "react";
+import ReactModal from "react-modal";
 import ReactDOM from "react-dom";
 import { render, fireEvent } from "@testing-library/react";
-
-// this is only here for HMR/codesandbox purposes
-// in a real scenario, you'd probably just do the stuff
-// that's in the if statement.
-let modalRoot = document.getElementById("modal-root") as HTMLDivElement;
-if (!modalRoot) {
-  modalRoot = document.createElement("div");
-  modalRoot.setAttribute("id", "modal-root");
-  document.body.appendChild(modalRoot);
-}
-
-interface ModalProps {
-  onClose: () => void;
-}
-
-class Modal extends React.Component<ModalProps> {
-  el = document.createElement("div");
-  componentDidMount() {
-    modalRoot.appendChild(this.el);
-  }
-  componentWillUnmount() {
-    modalRoot.removeChild(this.el);
-  }
-  render() {
-    return ReactDOM.createPortal(
-      <div
-        style={{
-          position: "absolute",
-          top: "0",
-          bottom: "0",
-          left: "0",
-          right: "0",
-          display: "grid",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "rgba(0,0,0,0.3)"
-        }}
-        onClick={this.props.onClose}
-      >
-        <div
-          style={{
-            padding: 20,
-            background: "#fff",
-            borderRadius: "2px",
-            display: "inline-block",
-            minHeight: "300px",
-            margin: "1rem",
-            position: "relative",
-            minWidth: "300px",
-            boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
-            justifySelf: "center"
-          }}
-          onClick={e => e.stopPropagation()}
-        >
-          {this.props.children}
-          <hr />
-          <button onClick={this.props.onClose}>Close</button>
-        </div>
-      </div>,
-      this.el
-    );
-  }
-}
+import "@testing-library/jest-dom/extend-expect";
 
 const App = () => {
   const [isShowModal, setIsShowModal] = useState(false);
@@ -72,28 +11,49 @@ const App = () => {
 
   return (
     <>
-      {isShowModal && (
-        <Modal onClose={toggleShowModal}>
-          <div>Hello! I'm a modal</div>
-        </Modal>
-      )}
+      {isShowModal && <ExampleApp />}
+
       <button onClick={toggleShowModal}>Toggle Modal</button>
     </>
   );
 };
 
+class ExampleApp extends React.Component {
+  state = {
+    showModal: false
+  };
+
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    return (
+      <>
+        <button id="button" onClick={this.handleOpenModal}>
+          Trigger Modal
+        </button>
+        <ReactModal
+          isOpen={this.state.showModal}
+          contentLabel="Minimal Modal Example"
+        >
+          <div>
+            <button onClick={this.handleCloseModal}>Close Modal</button>
+            <h1>Hello! I'm Modal</h1>
+          </div>
+        </ReactModal>
+      </>
+    );
+  }
+}
+
 test("modal shows the children and a close button", () => {
-  // Act
-  const { debug, queryByText } = render(<App />);
-
-  const button = queryByText("Toggle Modal") as HTMLButtonElement;
-
-  expect(button).toBeTruthy();
-
-  fireEvent.click(button);
-
-  expect(queryByText("Hello! I'm a modal")).toBeTruthy();
-
-  fireEvent.click(queryByText(/close/i) as HTMLButtonElement);
-  expect(queryByText("Hello! I'm a modal")).toBeFalsy();
+  const { getByText } = render(<App />);
+  fireEvent.click(getByText(/Toggle Modal/i));
+  fireEvent.click(getByText(/Trigger Modal/i));
+  expect(getByText("Hello! I'm Modal")).toBeTruthy();
 });
