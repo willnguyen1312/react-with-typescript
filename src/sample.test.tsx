9d3a204418cfa2f6b164322bc15f0296305e5a39
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-11-27 12:09:00 +0700

    Tada test

--- src/sample.test.tsx
+++ src/sample.test.tsx
@@ -4,8 +4,18 @@ import { getFactor } from "./getFactor";
 jest.mock("./getFactor");
 
 describe("Sample test", () => {
-  it("should work", () => {
-    (getFactor as jest.Mock<any>).mockReturnValueOnce(20);
+  it("should work first", () => {
+    (getFactor as jest.Mock).mockReturnValueOnce(20);
     expect(run()).toBe(20);
+  });
+
+  it("should work second", () => {
+    (getFactor as jest.Mock).mockReturnValueOnce(30);
+    expect(run()).toBe(30);
+  });
+
+  it("should work last", () => {
+    (getFactor as jest.Mock).mockReturnValueOnce(100);
+    expect(run()).toBe(100);
   });
 });
