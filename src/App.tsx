3bd9683d7588340fcab923952c34bfd8435f0260
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-01-12 13:40:32 +0700

    Voila Greenlet Worker

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,23 @@ import React, { FC } from "react";
 // import Playground from "./components/Playground";
 import Sample from "./components/Sample";
 import PDF from "./pdf";
+import greenlet from "greenlet";
+import { calculatePrimes } from "./worker";
 // import Slider from "./components/Slider";
 // import cover from "./axo.jpg";
 
 const Hello = () => <h1>Hello</h1>;
 
 const Hi: FC<{ AAA: any }> = ({ AAA }) => <div>{AAA}</div>;
+
+// @ts-ignore
+const getNum = greenlet(calculatePrimes);
+
+(async () => {
+  const result = await getNum(1000, 1000000000);
+  // const result = await calculatePrimes(1000, 1000000000);
+  console.log(result);
+})();
 
 const App = () => {
   return (
---
+++ src/worker/index.ts
@@ -0,0 +1,20 @@
+export const calculatePrimes = (iterations: number, multiplier: number) => {
+  let primes = [];
+  for (let i = 0; i < iterations; i++) {
+    let candidate = i * (multiplier * Math.random());
+    let isPrime = true;
+
+    for (var c = 2; c <= Math.sqrt(candidate); ++c) {
+      if (candidate % c === 0) {
+        // not prime
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(candidate);
+    }
+  }
+
+  return primes;
+};
